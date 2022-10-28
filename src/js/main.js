e60f7ea6bd01e6d59c80a8e3bb724cc8ff62c88a
Tomasz Rymarczyk <tomako0@gmail.com>
2022-10-28 20:54:44 +0200

    craete content appearing after clicking

--- src/js/main.js
+++ src/js/main.js
@@ -6,18 +6,31 @@ const navBtnBars = document.querySelector('.burger-btn__bars')
 const daysCount = document.querySelector('.days__count')
 const hoursCount = document.querySelector('.hours__count')
 const minutesCount = document.querySelector('.minutes__count')
-const secondsCount = document.querySelector('.seconds__count');
+const secondsCount = document.querySelector('.seconds__count')
+
+const header = document.querySelector('.header')
+const headerBtn = document.querySelectorAll('.homeBtn')
+const info = document.querySelector('.info')
+const infoBtn = document.querySelectorAll('.QandABtn')
+const contact = document.querySelector('.contact')
+const map = document.querySelector('.map')
+const plan = document.querySelector('.plan')
+const planBtn = document.querySelectorAll('.planBtn')
+const menu = document.querySelector('.menu')
+const menuBtn = document.querySelectorAll('.menuBtn')
+const photos = document.querySelector('.photos')
+const photosBtn = document.querySelectorAll('.photosBtn')
 
 const handleNav = () => {
 	nav.classList.toggle('nav--active')
 
-	navBtnBars.classList.remove('black-bars-color')
+	// navBtnBars.classList.remove('black-bars-color')
 
-	allNavItems.forEach(item => {
-		item.addEventListener('click', () => {
-			nav.classList.remove('nav--active')
-		})
-	})
+	// allNavItems.forEach(item => {
+	// 	item.addEventListener('click', () => {
+	// 		nav.classList.remove('nav--active')
+	// 	})
+	// })
 
 	handleNavItemsAnimation()
 }
@@ -43,7 +56,6 @@ const setTime = () => {
 	daysCount.textContent = days
 	hoursCount.textContent = hours
 	minutesCount.textContent = minutes
-	
 }
 
 const appUpdate = () => {
@@ -51,6 +63,62 @@ const appUpdate = () => {
 	setTime()
 }
 
+const handleHome = () => {
+	header.classList.remove('header--disable')
+	info.classList.add('info--disable')
+	map.classList.add('map--disable')
+	contact.classList.add('contact--disable')
+	plan.classList.add('plan--disable')
+	menu.classList.add('menu--disable')
+	photos.classList.add('photos--disable')
+	nav.classList.remove('nav--active')
+}
+const handleInfo = () => {
+	header.classList.add('header--disable')
+	info.classList.remove('info--disable')
+	map.classList.remove('map--disable')
+	contact.classList.remove('contact--disable')
+	plan.classList.add('plan--disable')
+	menu.classList.add('menu--disable')
+	photos.classList.add('photos--disable')
+	nav.classList.remove('nav--active')
+}
+const handlePlan = () => {
+	header.classList.add('header--disable')
+	info.classList.add('info--disable')
+	map.classList.add('map--disable')
+	contact.classList.add('contact--disable')
+	plan.classList.remove('plan--disable')
+	menu.classList.add('menu--disable')
+	photos.classList.add('photos--disable')
+	nav.classList.remove('nav--active')
+}
+const handleMenu = () => {
+	header.classList.add('header--disable')
+	info.classList.add('info--disable')
+	map.classList.add('map--disable')
+	contact.classList.add('contact--disable')
+	plan.classList.add('plan--disable')
+	menu.classList.remove('menu--disable')
+	photos.classList.add('photos--disable')
+	nav.classList.remove('nav--active')
+}
+const handlePhotos = () => {
+	header.classList.add('header--disable')
+	info.classList.add('info--disable')
+	map.classList.add('map--disable')
+	contact.classList.add('contact--disable')
+	plan.classList.add('plan--disable')
+	menu.classList.add('menu--disable')
+	photos.classList.remove('photos--disable')
+	nav.classList.remove('nav--active')
+}
+
 navBtn.addEventListener('click', handleNav)
 appUpdate()
 setInterval(setTime, 1000)
+headerBtn.forEach(btn => btn.addEventListener('click', handleHome))
+infoBtn.forEach(btn => btn.addEventListener('click', handleInfo))
+menuBtn.forEach(btn => btn.addEventListener('click', handlePlan))
+planBtn.forEach(btn => btn.addEventListener('click', handleMenu))
+photosBtn.forEach(btn => btn.addEventListener('click', handlePhotos))
